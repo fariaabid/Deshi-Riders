e2ca6f2163df974b5bb096af5b9795f49ef34210
faria abid <faria.ju.2k16@gmail.com>
2021-03-20 19:23:48 +0600

    section added

--- src/components/VehicleDetails/VehicleDetails.js
+++ src/components/VehicleDetails/VehicleDetails.js
@@ -2,38 +2,39 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import "./VehicleDetails.css";
-import icon from "../../images/icons/peopleicon.png";
 
 const VehicleDetails = (props) => {
   const { vehicleId, img, vehicleName, vehicleFare, key, seat } = props.vehicle;
 
   return (
-    // <div className="my-5 d-flex justify-content-between align-items-center">
-    //     <div className="col-md-5">
-    //         <img src={img} alt="hotel" className="img-fluid" />
-    //     </div>
-    //     <div className="col-md-7 hotel-info">
-    //         <h5>{vehicleName}</h5>
-    //         <p>{seat}</p>
-    //         <p>{vehicleFare}</p>
-
-    //     </div>
-    // </div>
-    <div className="my-1 d-flex justify-content-between align-items-center bg-secondary">
+    <div className="my-4 d-flex justify-content-between align-items-center bg-secondary text-white divHeight">
       <div className="col-md-3">
         <img src={img} alt="hotel" className="img-fluid" />
       </div>
       <div className="col-md-2">
         <h5>{vehicleName}</h5>
       </div>
-      <div className="col-md-2">
-          <img src="icon" alt=""/>
+      {/* <div className="col-md-5 icon">
+          <img src="../../images/icons/peopleicon.png" alt=""/>
       </div>
       <div className="col-md-1">
         <p>{seat}</p>
+      </div> */}
+      {/* <div className="col-md-3">
+        <div className="row d-flex">
+          <div className="col-md-6">
+            <img src="../../images/icons/peopleicon.png" alt="" />
+          </div>
+          <div className="col-md-6">
+            <p>{seat}</p>
+          </div>
+        </div>
+      </div> */}
+      <div className="col-md-3">
+      <h5>Seat:{seat}</h5>
       </div>
       <div className="col-md-4">
-        <p>${vehicleFare}</p>
+        <h5>${vehicleFare}</h5>
       </div>
     </div>
   );
